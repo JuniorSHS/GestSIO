9693557b6763b775f59c045206579115102e2ed4
Junior <61574965+JuniorSHS@users.noreply.github.com>
2020-09-11 16:14:33 +0400

    Update ViewEtudiantActivity.java

--- main/java/fr/hassanijunior/gestsio/ViewEtudiantActivity.java
+++ main/java/fr/hassanijunior/gestsio/ViewEtudiantActivity.java
@@ -9,19 +9,44 @@ import androidx.appcompat.app.AppCompatActivity;
 public class ViewEtudiantActivity extends AppCompatActivity {
 
     private TextView txtnomEtudiant;
+    private TextView txtprenomEtudiant;
+    private TextView txtanniversaireEtudiant;
+    private TextView txtadresseEtudiant;
+    private TextView txtadresseEtudiant2;
+    private TextView txtadresseEtudiant3;
+    private TextView txttelEtudiant;
+    private TextView txtemailEtudiant;
+    private TextView txtobservationEtudiant;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_etudiant);
         txtnomEtudiant = (TextView) findViewById(R.id.nomEtudiant);
+        txtprenomEtudiant = (TextView) findViewById(R.id.prenomEtudiant);
+        txtanniversaireEtudiant = (TextView) findViewById(R.id.anniversaireEtudiant);
+        txtadresseEtudiant = (TextView) findViewById(R.id.adresseEtudiant);
+        txtadresseEtudiant2 = (TextView) findViewById(R.id.adresseEtudiant2);
+        txtadresseEtudiant3 = (TextView) findViewById(R.id.adresseEtudiant3);
+        txttelEtudiant = (TextView) findViewById(R.id.telEtudiant);
+        txtemailEtudiant = (TextView) findViewById(R.id.emailEtudiant);
+        txtobservationEtudiant = (TextView) findViewById(R.id.observationEtudiant);
+
     }
 
     @Override
     protected void onResume() {
         super.onResume();
 
-        txtnomEtudiant.setText("DURAND");
+        txtnomEtudiant.setText("SAID HASSANI");
+        txtprenomEtudiant.setText("SAID JUNIOR");
+        txtanniversaireEtudiant.setText("19/12/1995");
+        txtadresseEtudiant.setText("12 aller coco");
+        txtadresseEtudiant2.setText("97400");
+        txtadresseEtudiant3.setText("Saint Denis");
+        txttelEtudiant.setText("0693123456");
+        txtemailEtudiant.setText("JuniorTheBoss@gmail.com");
+        txtobservationEtudiant.setText("Bon élève.");
 
     }
 }
